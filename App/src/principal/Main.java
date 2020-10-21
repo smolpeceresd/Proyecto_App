bebd45f4d3b25d3cdbe06f110fc6fb53de6af783
smolpeceresd <54994511+smolpeceresd@users.noreply.github.com>
2020-10-21 22:02:44 +0200

    Update Main.java

--- App/src/principal/Main.java
+++ App/src/principal/Main.java
@@ -22,14 +22,14 @@ public class Main {
 		Scanner sc= new Scanner(System.in);
 		System.out.println("\tBienvenido a \"empresa\"");
 
-		System.out.println("\t¿Como vas a usar la app? \n 1 Como usuario \n 2 Como empresa\n 3 Para salir");
-		System.out.print("\t\tElección: "); eleccion=sc.nextInt();
+		System.out.println("\tÂ¿Como vas a usar la app? \n 1 Como usuario \n 2 Como empresa\n 3 Para salir");
+		System.out.print("\t\tElecciÃ³n: "); eleccion=sc.nextInt();
 		if(eleccion==3) {sc.close();};
 		return eleccion;
 	}
 
 	public static boolean registrado() {
-		return decisiones("\t\t¿Estas registrado?");
+		return decisiones("\t\tÂ¿Estas registrado?");
 	}
 
 	public static boolean decisiones(String a) {
@@ -48,10 +48,10 @@ public class Main {
 		}
 	}
 	public static String descripcion() {
-		if(decisiones("¿Quieres añadir una descripción?")==true) {
+		if(decisiones("Â¿Quieres aÃ±adir una descripciÃ³n?")==true) {
 			@SuppressWarnings("resource")
 			Scanner sc= new Scanner(System.in);
-			System.out.print("Descripción:");
+			System.out.print("DescripciÃ³n:");
 			String descripcion=sc.nextLine();
 			return descripcion;
 		}else {
@@ -81,26 +81,26 @@ public class Main {
 	//HOTEL
 
 	public static boolean aparcamiento() {
-		return decisiones("¿Tu hotel tiene aparcameinto?");
+		return decisiones("Â¿Tu hotel tiene aparcameinto?");
 	}
-	public static boolean niños() {
-		return decisiones("¿Tu hotel Acepta niños?");		
+	public static boolean niÃ±os() {
+		return decisiones("Â¿Tu hotel Acepta niÃ±os?");		
 	}
 	@SuppressWarnings("resource")
 	public static int menuHotelEdition() {
 		Scanner sc=new Scanner(System.in);
-		System.out.print("\n¿Que desea modificar?\n"
+		System.out.print("\nÂ¿Que desea modificar?\n"
 				+ "1 Numero de Habitaciones\n"+
 				"2 Nombre del hotel\n"+
-				"3 La contraseña del hotel\n"+
+				"3 La contraseÃ±a del hotel\n"+
 				"4 El telefono del hotel\n"+
 				"5 La direccion del hotel\n"+
 				"6 El aparcamiento\n"+
-				"7 Aceptación de niños\n"+
+				"7 AceptaciÃ³n de niÃ±os\n"+
 				"8 El numero de estrellas\n"+
 				"9 La piscina del hotel\n"+
 				"10 El restaurante\n"+
-				"11 La aceptación de las mascotas\n"+
+				"11 La aceptaciÃ³n de las mascotas\n"+
 				"12 Para ver las valoraciones\n"+
 				"13 Para salir\n"+
 				"Eleccion: ");
@@ -118,7 +118,7 @@ public class Main {
 	}
 
 	public static Piscina pisciSuit() {
-		if(decisiones("¿Esta habitacion tiene piscina?")==true) {
+		if(decisiones("Â¿Esta habitacion tiene piscina?")==true) {
 			Piscina pisci=new Piscina(dimensiones(),climatizada());
 			return pisci;
 		}else {
@@ -139,13 +139,13 @@ public class Main {
 		return a;
 	}
 	public static boolean climatizada() {
-		return decisiones("¿La pisicina esta climatizada?");
+		return decisiones("Â¿La pisicina esta climatizada?");
 	}
 	public static boolean toallas() {
-		return decisiones("¿La pisicina tiene servicio de toallas?");
+		return decisiones("Â¿La pisicina tiene servicio de toallas?");
 	}
 	public static boolean bar() {
-		return decisiones("¿La pisicina tiene bar?");
+		return decisiones("Â¿La pisicina tiene bar?");
 	}
 
 	public static int menuPiscinaEdition(){
@@ -176,20 +176,20 @@ public class Main {
 	}
 
 	public static boolean desayuno() {
-		return decisiones("¿El restaurante tiene servicio de desayuno?");
+		return decisiones("Â¿El restaurante tiene servicio de desayuno?");
 	}
 	public static boolean comida() {
-		return decisiones("¿El restaurante tiene servicio de comida?");
+		return decisiones("Â¿El restaurante tiene servicio de comida?");
 	}
 	public static boolean cena() {
-		return decisiones("¿El restaurante tiene servicio de cena?");
+		return decisiones("Â¿El restaurante tiene servicio de cena?");
 	}
 
 	public static int menuRestauranteEdition() {
 		@SuppressWarnings("resource")
 		Scanner sc=new Scanner(System.in);
 		System.out.print("\nQue vamos a modificar?\n"
-				+ "1 La descripción\n"
+				+ "1 La descripciÃ³n\n"
 				+ "2 El servico de desayuno\n"
 				+ "3 El servicio de comida\n"
 				+ "4 El servicio de Cena\n"
@@ -201,7 +201,7 @@ public class Main {
 
 	//Mascotas
 	public static Mascotas mascotas() {
-		Mascotas mascotas=new Mascotas(decisiones("¿Tu hotel cuenta con guarderia para mascotas?"));
+		Mascotas mascotas=new Mascotas(decisiones("Â¿Tu hotel cuenta con guarderia para mascotas?"));
 		return mascotas;
 	}
 
@@ -231,7 +231,7 @@ public class Main {
 	public static int edad() {
 		@SuppressWarnings("resource")
 		Scanner sc= new Scanner(System.in);
-		System.out.print("Dime tu edad , recuerda que no se podrá cambiar, y has de ser +18:");
+		System.out.print("Dime tu edad , recuerda que no se podrÃ¡ cambiar, y has de ser +18:");
 		int edad=sc.nextInt();
 		while(edad<18) {
 			System.out.println("Tienes que ser >=18");
@@ -249,7 +249,7 @@ public class Main {
 	}
 
 	public static int menuUsuario() {
-		System.out.println("\n¿Que quieres hacer?\n"
+		System.out.println("\nÂ¿Que quieres hacer?\n"
 				+ "1 Para ver tus reservas\n"
 				+ "2 Para hacer una nueva reserva\n"
 				+ "3 para salir\n"
@@ -266,13 +266,13 @@ public class Main {
 		while(eleccion!=3) {
 			if(eleccion==1) {
 				if(registrado()==true) {
-					int perfil=data.usuarioRegistrado(datoString("Dime tu nombre de usuario"),datoString("Dime tu contraseña"));
+					int perfil=data.usuarioRegistrado(datoString("Dime tu nombre de usuario"),datoString("Dime tu contraseÃ±a"));
 					if(perfil!=-1) {
 						int decision=menuUsuario();
 						while(decision!=3) {
 							if(decision==1) {// ver tus reservas
-								if(decisiones("¿Deseas modificar alguna?")==true) {
-									if(decisiones("¿Deseas eliminarla?")==true) {
+								if(decisiones("Â¿Deseas modificar alguna?")==true) {
+									if(decisiones("Â¿Deseas eliminarla?")==true) {
 										int reserva=data.getVectorUsuario().get(perfil).eligeReserva();// poss de la reserva a gestionar
 
 										//borramos la reserva en el hotel
@@ -299,26 +299,26 @@ public class Main {
 								do {
 									int seleccionHotel=data.seleccionHotel();
 									System.out.println((data.getVectorHoteles().get(seleccionHotel).toString()));
-									if(decisiones("¿Deseas hacer una reserva?")==true) {
+									if(decisiones("Â¿Deseas hacer una reserva?")==true) {
 										int room=data.seleccionaHabitacion(data.getVectorHoteles().get(seleccionHotel));
 										//a hacer las competentes reservas->
-										data.getVectorHoteles().get(seleccionHotel).añadeReserva(
+										data.getVectorHoteles().get(seleccionHotel).aÃ±adeReserva(
 												new Reserva(
 														data.getVectorUsuario().get(perfil),//le paso el perfil
 														data.getVectorHoteles().get(seleccionHotel).getRooms().get(room))//le paso la habitacion
 												);
-										data.getVectorUsuario().get(perfil).añadeReserva(
+										data.getVectorUsuario().get(perfil).aÃ±adeReserva(
 												new Reserva(
 														data.getVectorHoteles().get(seleccionHotel),
 														data.getVectorHoteles().get(seleccionHotel).getRooms().get(room))
 												);										
 									}
-								}while(decisiones("¿Desea continuar en este hotel?")==true);
+								}while(decisiones("Â¿Desea continuar en este hotel?")==true);
 							}
 							decision=menuUsuario();
 						}
 					}else{
-						System.out.println("\n\n----Algo ha ido mal\nEl nombre de usuario o la contraseña ha sido mal introducido\n\n");
+						System.out.println("\n\n----Algo ha ido mal\nEl nombre de usuario o la contraseÃ±a ha sido mal introducido\n\n");
 					}
 				}else {
 					System.out.println("\nVamos a crear tu perfil");
@@ -328,17 +328,17 @@ public class Main {
 									datoString("Dime la combinacion de tu tarjeta"),
 									datoString("Dime tu correo"),
 									datoString("Dime tu telefono de contacto"),
-									datoString("Te vamos a registrar.\nDinos una contraseña para que puedas acceder a tus datos"),
+									datoString("Te vamos a registrar.\nDinos una contraseÃ±a para que puedas acceder a tus datos"),
 									dni());
-					System.out.println("\n\n\nAsí quedaría tu perfil\n");
+					System.out.println("\n\n\nAsÃ­ quedarÃ­a tu perfil\n");
 					System.out.println(perfil.toString());
-					data.añadeUsuario(perfil);
-					System.out.println("\n\n\n\n!!!TODO LISTO¡¡¡\n\n");
+					data.aÃ±adeUsuario(perfil);
+					System.out.println("\n\n\n\n!!!TODO LISTOÂ¡Â¡Â¡\n\n");
 				}
 			}else if( eleccion==2) {
-				System.out.println("¿Estas registrado?");
+				System.out.println("Â¿Estas registrado?");
 				if(registrado()==true){
-					int hotel=data.hotelRegistrado(datoString("Dime el nombre de tu hotel"),datoString("Dime tu contraseña"));
+					int hotel=data.hotelRegistrado(datoString("Dime el nombre de tu hotel"),datoString("Dime tu contraseÃ±a"));
 					if(hotel!=-1) {//si estas registrado
 						System.out.println("\n\nEste es tu perfil\n\n"+data.getVectorHoteles().get(hotel).toString()+"\n\n");
 						int edicionMenu=menuHotelEdition();
@@ -346,17 +346,17 @@ public class Main {
 							switch(edicionMenu) {
 							case 1:data.getVectorHoteles().get(hotel).setNumeroHabitaciones(cantidad("numero de habitaciones"));break;
 							case 2:data.getVectorHoteles().get(hotel).setNombreHotel(datoString("Dime el nombre del hotel, este es el que usaras para registrarte:"));break;
-							case 3:data.getVectorHoteles().get(hotel).setContraseñaHotel(datoString("Dinos una contraseña para que puedas acceder a tus datos"));break;
+							case 3:data.getVectorHoteles().get(hotel).setContraseÃ±aHotel(datoString("Dinos una contraseÃ±a para que puedas acceder a tus datos"));break;
 							case 4:data.getVectorHoteles().get(hotel).setTelefono(datoString("Dime el numero de telefono de tu hotel"));break;
 							case 5:data.getVectorHoteles().get(hotel).setDireccion(datoString("Dime la direccion del hotel"));break;
 							case 6:data.getVectorHoteles().get(hotel).setAparcamiento(aparcamiento());break;
-							case 7:data.getVectorHoteles().get(hotel).setNiños(niños());break;
+							case 7:data.getVectorHoteles().get(hotel).setNiÃ±os(niÃ±os());break;
 							case 8:data.getVectorHoteles().get(hotel).setNumeroEstrellas(cantidad("numero de estrellas de tu hotel"));break;
 							case 9:
 							{
 								if(data.getVectorHoteles().get(hotel).getPisci()==null) {
 									data.getVectorHoteles().get(hotel).setPisci(piscina());//la creas
-								}else if(decisiones("¿Quieres eliminar tu piscina?")==false){
+								}else if(decisiones("Â¿Quieres eliminar tu piscina?")==false){
 									int edicionPiscina=menuPiscinaEdition();
 									while(edicionPiscina!=5) {
 										switch(edicionPiscina) {
@@ -364,7 +364,7 @@ public class Main {
 										case 2:data.getVectorHoteles().get(hotel).getPisci().setClimatizada(climatizada());break;
 										case 3:data.getVectorHoteles().get(hotel).getPisci().setServicioToallas(toallas());break;
 										case 4:data.getVectorHoteles().get(hotel).getPisci().setBarPiscina(bar());break;
-										case 5:System.out.println("\n\n!!TODO LISTO¡¡\n");break;
+										case 5:System.out.println("\n\n!!TODO LISTOÂ¡Â¡\n");break;
 										default:System.out.println("\n\nAlgo ha ido mal.Prueba de nuevo\n");
 										}
 										System.out.println("\n\nEste es tu perfil\n\n"+data.getVectorHoteles().get(hotel).toString()+"\n\n");
@@ -379,7 +379,7 @@ public class Main {
 							{
 								if(data.getVectorHoteles().get(hotel).getRestaurante()==null) {
 									data.getVectorHoteles().get(hotel).setRestaurante(restaurante());
-								}else if(decisiones("¿Quieres eliminar tu restaurante?")==false){
+								}else if(decisiones("Â¿Quieres eliminar tu restaurante?")==false){
 									System.out.println(data.getVectorHoteles().get(hotel).getRestaurante().toString());
 									int edicionRestaurante=menuRestauranteEdition();
 									while(edicionRestaurante!=5) {
@@ -388,7 +388,7 @@ public class Main {
 										case 2:data.getVectorHoteles().get(hotel).getRestaurante().setDesayuno(desayuno());break;
 										case 3:data.getVectorHoteles().get(hotel).getRestaurante().setComida(comida());break;
 										case 4:data.getVectorHoteles().get(hotel).getRestaurante().setCena(cena());break;
-										case 5:System.out.println("\n\n!!!TODO LISTO¡¡¡\n");break;
+										case 5:System.out.println("\n\n!!!TODO LISTOÂ¡Â¡Â¡\n");break;
 										default: System.out.println("\nAlgo ha ido mal. prueba otra vez");
 										}
 										System.out.println(data.getVectorHoteles().get(hotel).getRestaurante().toString());
@@ -403,14 +403,14 @@ public class Main {
 							{
 								if(data.getVectorHoteles().get(hotel).getMascotas()==null) {
 									data.getVectorHoteles().get(hotel).setMascotas(mascotas());;
-								}else if(decisiones("¿Quieres eliminar tu guarderia?")==false){
-									data.getVectorHoteles().get(hotel).getMascotas().setGuarderia(decisiones("¿Tu hotel cuenta con guarderia para mascotas?"));
+								}else if(decisiones("Â¿Quieres eliminar tu guarderia?")==false){
+									data.getVectorHoteles().get(hotel).getMascotas().setGuarderia(decisiones("Â¿Tu hotel cuenta con guarderia para mascotas?"));
 								}else {
 									data.getVectorHoteles().get(hotel).setMascotas(null);
 								}
 							}break;
 							case 12:System.out.println(data.getVectorHoteles().get(hotel).getValoraciones().toString());break ;
-							case 13:System.out.println("\n!!!Todo LISTO¡¡¡\n\n");break;
+							case 13:System.out.println("\n!!!Todo LISTOÂ¡Â¡Â¡\n\n");break;
 							default:System.out.println("\n\nAlgo aha ido mal.Prueba de nuevo\n");
 							}
 							System.out.println("\n\n---------------------------------------------------------------------------\n"
@@ -419,7 +419,7 @@ public class Main {
 							edicionMenu=menuHotelEdition();
 						}
 					}else{
-						System.out.println("\n\n----Algo ha ido mal\nEl nombre de usuario o la contraseña ha sido mal introducido---\n\n");
+						System.out.println("\n\n----Algo ha ido mal\nEl nombre de usuario o la contraseÃ±a ha sido mal introducido---\n\n");
 					}
 				}else{
 					System.out.println("\nVamos a hacer la plantilla de tu Hotel\n");
@@ -427,58 +427,58 @@ public class Main {
 							(datoString("Dime el nombre del hotel, este es el que usaras para registrarte"),
 									datoString("Dime el numero de telefono de tu hotel"),
 									datoString("Dime la direccion del hotel"),
-									aparcamiento(),niños(),// son funciones bool
-									datoString("Te vamos a registrar.\nDinos una contraseña para que puedas acceder a tus datos")
+									aparcamiento(),niÃ±os(),// son funciones bool
+									datoString("Te vamos a registrar.\nDinos una contraseÃ±a para que puedas acceder a tus datos")
 									);
-					if(decisiones("¿Tu hotel tiene piscina?")==true) {
+					if(decisiones("Â¿Tu hotel tiene piscina?")==true) {
 						hotel.setPisci(piscina());
 					}
-					if(decisiones("¿Tu hotel tiene restaurante?")==true) {
+					if(decisiones("Â¿Tu hotel tiene restaurante?")==true) {
 						hotel.setRestaurante(restaurante());
 					}
-					if(decisiones("¿Tu hotel admite mascotas?")==true) {
+					if(decisiones("Â¿Tu hotel admite mascotas?")==true) {
 						hotel.setMascotas(mascotas());
 					}
 					System.out.println("\nVamos a registrar las diferentes habitaciones que tienes");
-					if(decisiones("¿Tienes Suits en tu hotel?")==true){
+					if(decisiones("Â¿Tienes Suits en tu hotel?")==true){
 						int a=cantidad("Dime cuantas suits diferentes tienes: ");
 						for(int i=0; i<a;i++) {
 							System.out.println("Vamos con la suit numero "+(i+1));
 							Habitacion habitacion= new Suit
 									(descripcion(),
 											cantidad("el numero de camas"),
-											decisiones("¿Tiene TV?"),
-											decisiones("¿Tiene wifi?"),
-											decisiones("¿Tiene minibar?"),
-											decisiones("¿Tiene Caja fuerte?"),
-											decisiones("¿Esta adaptada para minusvalidos?"),
+											decisiones("Â¿Tiene TV?"),
+											decisiones("Â¿Tiene wifi?"),
+											decisiones("Â¿Tiene minibar?"),
+											decisiones("Â¿Tiene Caja fuerte?"),
+											decisiones("Â¿Esta adaptada para minusvalidos?"),
 											precio(),
-											decisiones("¿Tiene Jacuzzi?"),
-											decisiones("¿Tiene una barra de bar personal?"),
-											cantidad("¿Cuntas habitaciones como esta hay?"),
+											decisiones("Â¿Tiene Jacuzzi?"),
+											decisiones("Â¿Tiene una barra de bar personal?"),
+											cantidad("Â¿Cuntas habitaciones como esta hay?"),
 											pisciSuit());
-							hotel.añade(habitacion);
+							hotel.aÃ±ade(habitacion);
 						}
 					}
 					System.out.println("\nVamos con las habitaciones mas estandar");
-					int b=cantidad("¿Cuantas habitaciones diferentes tienes?");
+					int b=cantidad("Â¿Cuantas habitaciones diferentes tienes?");
 					for(int i=0;i<b;i++) {
 						System.out.println("Vamos con la habitacion Estandar numero "+(i+1));
 						Habitacion habitacion=new Habitacion
 								(descripcion(),
 										cantidad("el numero de camas que tiene la habitacion"),
-										decisiones("¿Tiene Tv?"),
-										decisiones("¿Tiene wifi?"),
-										decisiones("¿Tiene minibar?"),
-										decisiones("¿Tiene Caja fuerte?"),
-										decisiones("¿Esta adaptada para minusvalidos?"),
+										decisiones("Â¿Tiene Tv?"),
+										decisiones("Â¿Tiene wifi?"),
+										decisiones("Â¿Tiene minibar?"),
+										decisiones("Â¿Tiene Caja fuerte?"),
+										decisiones("Â¿Esta adaptada para minusvalidos?"),
 										precio(),
-										cantidad("¿Cuantas habitaciones como esta hay?")
+										cantidad("Â¿Cuantas habitaciones como esta hay?")
 										);
-						hotel.añade(habitacion);
+						hotel.aÃ±ade(habitacion);
 					}
-					data.añadeHotel(hotel);
-					System.out.println("\n\n\n\n!!!TODO LISTO¡¡¡\n\n");
+					data.aÃ±adeHotel(hotel);
+					System.out.println("\n\n\n\n!!!TODO LISTOÂ¡Â¡Â¡\n\n");
 				}
 			}else if(eleccion!=1 && eleccion !=2){
 				System.out.println("\nPor favor introduce un dato valido\n");
