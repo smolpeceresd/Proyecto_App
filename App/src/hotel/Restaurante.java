13ae9415ea1665075bac7dbb79e45f1e66b93424
smolpeceresd <54994511+smolpeceresd@users.noreply.github.com>
2020-11-10 20:29:59 +0100

    Update Restaurante.java

--- App/src/hotel/Restaurante.java
+++ App/src/hotel/Restaurante.java
@@ -1,6 +1,7 @@
 package hotel;
 
-import java.util.Scanner;
+
+import traductor.Traductor;
 
 /**
  * @author Smolpeceresd
@@ -25,21 +26,21 @@ public class Restaurante {
 	}
 
 	//Metodos
-	public String toString() {
+	public String toString(Traductor diccionario) {
 		String deVuelta="";
 		if(this.getDescripcion()!=null) {
-			deVuelta+=("\n\t*Descripción:\n\t"+ this.getDescripcion());
+			deVuelta+=("\n\t*"+diccionario.getTexto("Descripcion_")+"\n\t"+ this.getDescripcion());
 		}
-		deVuelta+=("\n\t*Numero de Mesas: "+ this.getNumeroMesas());
-		deVuelta+=("\n\t*Servicios:\n");
+		deVuelta+=("\n\t*"+diccionario.getTexto("N_MESAS")+ this.getNumeroMesas());
+		deVuelta+=("\n\t*"+diccionario.getTexto("SERVICIOS_")+"\n");
 		if(this.isDesayuno()==true) {
-			deVuelta+=("\t\t* Desayuno");
+			deVuelta+=("\t\t* "+diccionario.getTexto("DESAYUNO"));
 		}
 		if(this.isComida()==true) {
-			deVuelta+=("\n\t\t* Comida");
+			deVuelta+=("\n\t\t* "+diccionario.getTexto("COMIDA"));
 		}
 		if(this.isCena()==true) {
-			deVuelta+=("\n\t\t* Cena");
+			deVuelta+=("\n\t\t* "+diccionario.getTexto("CENA"));
 		}
 		return deVuelta;
 	}
@@ -79,65 +80,4 @@ public class Restaurante {
 		this.numeroMesas = numeroMesas;
 	}
 
-	////////////////////////////////////////////////////////////////////Metodos Estandar
-	
-	
-	public static int menuEdicion() {
-		@SuppressWarnings("resource")
-		Scanner sc=new Scanner(System.in);
-		System.out.print("\n¿Que vamos a cambiar?\n"
-				+ "1 La descripcion\n"+
-				"2 Servicio de desayuno\n"+
-				"3 Servico de comida\n"+
-				"4 Servicio de cena\n"+
-				"5 Numero de mesas\n"+
-				"6 Para salir"+
-				"Eleccion: ");
-		int decision=sc.nextInt();
-		return decision;
-
-	}
-	public static String descripcion() {
-		if(decisiones("¿Quieres añadir una descripción?")==true) {
-			@SuppressWarnings("resource")
-			Scanner sc= new Scanner(System.in);
-			System.out.print("Descripción:");
-			String descripcion=sc.nextLine();
-			return descripcion;
-		}else {
-			return null;
-		}
-	}
-	public static boolean decisiones(String a) {
-		@SuppressWarnings("resource")
-		Scanner sc= new Scanner(System.in);
-
-		int eleccion;
-		do {
-			System.out.print("\n"+a+"\n\t 1 Si \n\t 2 No\n Eleccion:  ");
-			eleccion=sc.nextInt();
-		}while(eleccion!= 1 && eleccion !=2);
-		if(eleccion==1) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	public static int cantidad(String a)
-	{
-		int cantidad;
-		@SuppressWarnings("resource")
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Dime "+a+" : ");
-		cantidad=sc.nextInt();
-		return cantidad;
-	}
-	public static String datoString(String a)
-	{
-		@SuppressWarnings("resource")
-		Scanner sc=new Scanner(System.in);
-		System.out.print(a+" : ");
-		String dato=sc.nextLine();
-		return dato;
-	}
 }
